36072e4fc2b278774a011c7734349154a39eaf2d
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-06 17:22:04 +0700

    Update request

---
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize()
+    {
+        // Create middleware from kernel at here
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ],
+            'email' => [
+                'required', 'email', 'unique:users', 'max:255',
+            ],
+            'password' => [
+                'min:8', 'string', 'max:255', 'midedCase',
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+// This role only at update request
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize()
+    {
+        // Create middleware from kernel at here
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ],
+            'email' => [
+                'required', 'email', 'max:255', Rule::unique('users')->ignore($this->user),
+                //Rule unique only works for other record id
+            ],
+            'password' => [
+                'min:8', 'string', 'max:255', 'midedCase',
+            ],
+        ];
+    }
+}
