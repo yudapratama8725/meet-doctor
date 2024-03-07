2711270d75ec61f8b5aa247f0e277ab90a7edc1f
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-07 11:49:34 +0700

    Update and Fixing Group Route

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,19 @@ use App\Http\Controllers\Frontsite\PaymentController;
 
 Route::resource('/', LandingController::class);
 
-Route::group(['prefix' => 'backsite', 'as' => 'backsite.', 'middleware' =>['auth:sanctum', 'verified']],function () {
+Route::group(['middleware' =>['auth:sanctum', 'verified']],function () {
     
     //appointment page
     Route::resource('appointment', AppointmentController::class);
 
     //payment page
     Route::resource('payment', PaymentController::class);
+});
+
+Route::group(['prefix' => 'backsite', 'as' => 'backsite.', 'middleware' =>['auth:sanctum', 'verified']],function () {
+    
+    //Dashbaord Admin page
+    return view('dashboard');
 });
 
 // Route::get('/', function () {
