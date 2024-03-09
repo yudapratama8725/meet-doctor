86e23d5177730ce972f88b3ad370484333211ab4
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-03-09 09:31:51 +0700

    slicing page sign in, appointment and payment

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('includes.frontsite.meta');
+
+    <title>@yield('title') | Meet-Doctor</title>
+
+    @stack('before-style')
+        @include('includes.frontsite.style')
+    @stack('after-style')
+
+  </head>
+  <body>
+
+    @yield('content')
+
+    @stack('before-script')
+        @include('includes.frontsite.script')
+    @stack('after-script')
+
+    {{-- Modals --}}
+    {{-- If you have modals create here --}}
+
+  </body>
+</html>
